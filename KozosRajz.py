386a06c17ad94c2c860d951a588a65db69fec079
Matyolf <youcantstealmydata@gmail.com>
2026-05-13 10:28:01 +0200

    izzok

--- KozosRajz.py
+++ KozosRajz.py
@@ -9,3 +9,16 @@ import turtle
 #Test:
 
 #Izzok:
+turtle.teleport(0,0)
+colors=["green", "yellow", "red"]
+i=0
+while i<3:
+    turtle.fillcolor(colors[i%3])
+    turtle.color(colors[i%3])
+    turtle.begin_fill()
+    turtle.circle(100)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.setheading(90)
+    turtle.forward(150)
+    turtle.pendown()
