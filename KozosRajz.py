94c5193bf7d953e54409d394e37d024dfb74eee4
3lekes <davidelekes13@gmail.com>
2026-05-13 10:34:33 +0200

    lampatest_kesz

--- KozosRajz.py
+++ KozosRajz.py
@@ -7,5 +7,17 @@ import turtle
 #Talp:
 
 #Test:
+turtle.teleport(-50, 0)
+turtle.color("black")
+turtle.fillcolor("black")
+turtle.begin_fill()
+j=0
+while j<2:
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(225)
+    turtle.left(90)
+    j+=1
+turtle.end_fill()
 
 #Izzok:
