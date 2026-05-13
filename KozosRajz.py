11eaa65717fbb64b86558b70648065099197ff02
Matyolf <youcantstealmydata@gmail.com>
2026-05-13 10:33:31 +0200

    izzok_fix

--- KozosRajz.py
+++ KozosRajz.py
@@ -13,12 +13,16 @@ turtle.teleport(0,0)
 colors=["green", "yellow", "red"]
 i=0
 while i<3:
+    turtle.setheading(0)
     turtle.fillcolor(colors[i%3])
     turtle.color(colors[i%3])
     turtle.begin_fill()
-    turtle.circle(100)
+    turtle.circle(50)
     turtle.end_fill()
     turtle.penup()
     turtle.setheading(90)
-    turtle.forward(150)
+    turtle.forward(125)
     turtle.pendown()
+    i+=1
+
+turtle.done()
