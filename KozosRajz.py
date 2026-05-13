0b28427e8375129fe2dc8404ffb24d7baadbe4c1
doczybarni <doczybarni@gmail.com>
2026-05-13 10:59:55 +0200

    lampatalp_kesz

--- KozosRajz.py
+++ KozosRajz.py
@@ -2,9 +2,24 @@
 #Elekes David - lampatest
 #Dóczy Barnabás lámpatalp
 
+
 import turtle
 
 #Talp:
+turtle.teleport(-25,0)
+turtle.color("grey")
+turtle.fillcolor("grey")
+turtle.begin_fill()
+k=0
+while k<2:
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    k+=1
+turtle.end_fill()
+
+
 
 #Test:
 
